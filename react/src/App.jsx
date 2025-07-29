19fbf68e1f9a5570cc15698cc46ddc10cd45dfdd
Harikrishna <patimaharikrishnap@gmail.com>
2025-07-29 23:12:34 +0530

    added logout functionality

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,6 +6,7 @@ import RegisterForm from './components/RegisterForm'
 import BusList from './components/BusList'
 import BusSeats from './components/BusSeats'
 import UserBookings from './components/UserBookings'
+import Wrapper from './components/Wrapper'
 
 const App = () => {
 const [token,setToken] = useState(localStorage.getItem('token'))
@@ -16,15 +17,23 @@ const [userId,setUserId] = useState(localStorage.getItem('userId'))
     localStorage.setItem('token',token)
     localStorage.setItem('userId',userId)
   }
+
+  const handleLogout = ()=>{
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+  }
+
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<BusList/>}/>
-        <Route path='/register' element={<RegisterForm/>}/>
-        <Route path='/login' element={<LoginForm onLogin={handleLogin}/>}/>
-        <Route path='/bus/:busId' element={<BusSeats token={token}/>}/>
-        <Route path='/my-bookings' element={<UserBookings token={token} userId={userId}/>}/>
-      </Routes>
+      <Wrapper handleLogout={handleLogout} token={token}>
+        <Routes>
+          <Route path='/' element={<BusList/>}/>
+          <Route path='/register' element={<RegisterForm/>}/>
+          <Route path='/login' element={<LoginForm onLogin={handleLogin}/>}/>
+          <Route path='/bus/:busId' element={<BusSeats token={token}/>}/>
+          <Route path='/my-bookings' element={<UserBookings token={token} userId={userId}/>}/>
+        </Routes>
+      </Wrapper>
     </div>
   )
 }
