e3bf11467a33210f0a9e9a62f3def45191ece0ad
Harikrishna <patimaharikrishnap@gmail.com>
2025-07-29 17:09:06 +0530

    added cors origins from DRF

--- django/travels/travels/settings.py
+++ django/travels/travels/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware", # middlewares
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +63,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_CREDENTIALS: True
 ROOT_URLCONF = 'travels.urls'
 
 TEMPLATES = [
